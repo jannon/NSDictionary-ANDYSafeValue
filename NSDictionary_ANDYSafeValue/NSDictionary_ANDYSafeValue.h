34c29267703810b6dd7d04a9417a0ac4f9458614
Jannon Frank <jannon@cs.stanford.edu>
2016-07-02 03:07:58 -0700

    add shared scheme for Carthage

---
+++ NSDictionary_ANDYSafeValue/NSDictionary_ANDYSafeValue.h
@@ -0,0 +1,7 @@
+@import Foundation;
+
+FOUNDATION_EXPORT double NSDictionary_ANDYSafeValueVersionNumber;
+FOUNDATION_EXPORT const unsigned char NSDictionary_ANDYSafeValueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSDictionary_ANDYSafeValue/PublicHeader.h>
+#import "NSDictionary+ANDYSafeValue.h"
